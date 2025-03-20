df4653c9844cc04e59cdd797cb9f67dce0368a7f
iAmNsengi <iamnsengi@icloud.com>
2025-03-20 07:06:32 +0200

    ft: change all files to .ts

---
+++ types/index.ts
@@ -0,0 +1,34 @@
+import { Request } from "express";
+import { Document } from "mongoose";
+
+export interface IUser extends Document {
+  username: string;
+  email?: string;
+  phoneNumber?: string;
+  password?: string;
+  visibleName?: string;
+  hiddenName?: string;
+  gender?: "Male" | "Female" | "Non-Binary";
+  dob?: Date;
+  bio?: string;
+  interests?: {
+    professional: string[];
+    hobbies: string[];
+  };
+  isVerified: boolean;
+  isActive: boolean;
+  lastActive: Date;
+  maxSlots: number;
+  location?: {
+    type: string;
+    coordinates: number[];
+    formattedAddress?: string;
+  };
+  googleId?: string;
+  googleEmail?: string;
+  comparePassword(password: string): Promise<boolean>;
+}
+
+export interface AuthRequest extends Request {
+  user?: IUser;
+}
